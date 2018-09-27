33c9159edb90193cb32a3d0f685a320f301286d2
athensbird <markwang@utexas.edu>
2018-09-27 18:10:49 -0500

    initialize TaskDetail UI

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { Component }from 'react';
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import TextField from "@material-ui/core/TextField";
 
-const TaskDetail = (props) => {
-  return (
-    <h3>placeholder</h3>
-  );
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+
+};
+
+class TaskDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    const { task } = this.props;
+    this.state = {
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      completed: task.completed,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(field) {
+    return (event) => {
+      const value = event.target.value;
+      this.setState({
+        [field]: value,
+      });
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Chip>Back to Tasks</Chip>
+        <TextField
+          id="title"
+          value={this.state.title}
+          onChange={this.handleChange("title")}
+        />
+        <Button>Complete</Button>
+        <TextField
+          id="description"
+          value={this.state.description}
+          onChange={this.handleChange("description")}
+        />
+        <Button>Save</Button>
+        <Button>Cancel</Button>
+        <Button>Delete</Button>
+      </div>
+    );
+  }
 }
 
-export default TaskDetail;
+export default withStyles(styles)(TaskDetail);
