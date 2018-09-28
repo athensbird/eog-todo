dd968de46b2c91954fd2a1f9d1d40b2987989f91
athensbird <markwang@utexas.edu>
2018-09-27 23:28:19 -0500

    overhaul TaskDetail UI

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Card";
+import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 
 import { Link } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
-
+  complete: {
+    'background-color': '#E4E4E4',
+    'margin-left': '1rem',
+  },
+  save: {
+    'background-color': '#3FBAF9',
+    'margin-right': '1rem',
+  },
+  cancel: {
+    'background-color': '#E4E4E4',
+    'margin-right': '1rem',
+  },
+  delete: {
+    'background-color': '#E57373',
+    'margin-right': '1rem',
+  },
+  row: {
+    'margin-top': '1rem',
+    'border-radius': '0',
+    'box-shadow': 'none',
+  },
+  grid : {
+    margin: '10% 20%',
+    width: '60%',
+    flexGrow: '1',
+    'box-shadow': 'none',
+  },
+  title: {
+    width: '30%',
+  },
+  description: {
+    width: '60%',
+  },
 };
 
 class TaskDetail extends React.Component {
@@ -29,26 +62,37 @@ class TaskDetail extends React.Component {
   render() {
     const taskID = this.props.match.params.task_id;
     const task = this.props.tasks.filter(t => t.id === taskID);
+    const { classes } = this.props;
     return (
-      <Card>
-        {task && <div>
-          <Link to={"/task"}>Back to Tasks</Link>
-          <TextField
-            id="title"
-            value={this.state.task.title}
-            onChange={this.handleChange("title")}
-          />
-          <Button>Complete</Button>
-          <TextField
-            id="description"
-            value={this.state.task.description}
-            onChange={this.handleChange("description")}
-          />
-          <Button>Save</Button>
-          <Button>Cancel</Button>
-          <Button>Delete</Button>
-        </div>}
-      </Card>
+      <Grid className={classes.grid}>
+        {task && <Grid container="true">
+          <Paper className={classes.row}>
+            <Link to={"/task"}>Back to Tasks</Link>
+          </Paper>
+          <Paper className={classes.row}>
+            <TextField
+              id="title"
+              className={classes.title}
+              value={this.state.task.title}
+              onChange={this.handleChange("title")}
+            />
+            <Button className={classes.complete}>Complete</Button>
+          </Paper>
+          <Paper className={classes.row}>
+            <TextField
+              id="description"
+              className={classes.description}
+              value={this.state.task.description}
+              onChange={this.handleChange("description")}
+            />
+          </Paper>
+          <Paper className={classes.row}>
+              <Button className={classes.save}>Save</Button>
+              <Button className={classes.cancel}>Cancel</Button>
+              <Button className={classes.delete}>Delete</Button>
+          </Paper>
+        </Grid>}
+      </Grid>
     );
   }
 }
